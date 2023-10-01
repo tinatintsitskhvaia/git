01230983e6cf9e3b2954bc1f22dcb4bb86f1e688
S <s>
2023-10-01 19:06:34 +0400

    {"types":"C","runs":5}

--- problem03.java
+++ problem03.java
@@ -1,5 +1,11 @@
-
-
-public class problem03 {
-
+import stanford.karel.*;
+/* კარელი დგას 1x1 უჯრაზე, გაიყვანეთ იგი პირველი ქუჩის ბოლომდე. გაითვალისწინეთ, რომ
+სამყაროს ზომა არ იცით. თქვენი პროგრამა უნდა მუშაობდეს ნებისმიერი ზომის
+სამყაროსათვის.
+ */
+public class problem03 extends Karel{
+	public void run() {
+			if frontIsClear() {
+				move();
+			}
 }
