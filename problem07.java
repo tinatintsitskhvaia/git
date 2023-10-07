65d2f00c07f770d285baaf02325405823b4af114
S <s>
2023-10-07 18:34:33 +0400

    {"types":"C","runs":36}

--- problem07.java
+++ problem07.java
@@ -1,4 +1,45 @@
+import stanford.karel.*;
+public class problem07 extends Karel{
+	public void run() {
+		while(notFacingNorth()){
+			fillLine();
+			moveToNextLineIfPossible();
+		}
+	}
 
-public class problem07 {
+	private void moveToNextLineIfPossible() {
+		if(facingEast()&& leftIsClear()) {
+			turnLeft();
+			move();
+			turnLeft();
+		}else if(facingWest() && rightIsClear()){
+			turnRight();
+			move();
+			turnRight();
+		}else {
+			while(notFacingNorth()){
+				turnLeft();
+			}
+		}
+		
+	}
+
+	private void turnRight() {
+		for(int i = 0; i < 3; i++){
+			turnLeft();
+		}
+	}
+
+	private void fillLine() {
+		while(frontIsClear()){
+			if(noBeepersPresent()){
+				putBeeper();
+			}
+			move();
+		}
+		if(noBeepersPresent()){
+			putBeeper();
+		}
+	}
 
 }
