02f464f671a012d832b5c3664341cd8da9e93f88
S <s>
2023-10-07 18:10:07 +0400

    {"types":"C","runs":35}

--- problem06.java
+++ problem06.java
@@ -1,4 +1,73 @@
+import stanford.karel.SuperKarel;
 
-public class problem06 {
+public class problem06 extends SuperKarel {
+	public void run() {
+		move();
+		moveFirstPileToDestinationAndBackUp();
+		move();
+		moveSecondPileToDestinationAndBackUp();
+		restoreBeepers();
+		turnAround();
+		move();
+		turnAround();
+		restoreBeepers();
+	}
 
+	private void restoreBeepers() {
+		turnLeft();
+		move();
+		turnAround();
+		while(beepersPresent()) {
+			pickBeeper();
+			putBeeperOneCellAway();
+		}
+		move();
+		turnLeft();
+	}
+		
+		
+	private void backUpBeeper() {
+		turnLeft();
+		move();
+		putBeeper();
+		turnAround();
+		move();
+		turnLeft();		
+	}
+
+	private void putBeeperTwoCellsAway() {
+		move();
+		move();
+		putBeeper();
+		turnAround();
+		move();
+		move();
+		turnAround();
+	}
+
+	private void moveSecondPileToDestinationAndBackUp() {
+		while(beepersPresent()) {
+			pickBeeper();
+			putBeeperOneCellAway();
+			backUpBeeper();	
+		}
+		
+	}
+
+	private void putBeeperOneCellAway() {
+		move();
+		putBeeper();
+		turnAround();
+		move();
+		turnAround();	
+	}
+
+	private void moveFirstPileToDestinationAndBackUp() {
+		while(beepersPresent()) {
+			pickBeeper();
+			putBeeperTwoCellsAway();
+			backUpBeeper();
+		}
+		
+	}   
 }
