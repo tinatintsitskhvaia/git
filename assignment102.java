15f838bd5b6488c89ec587723fb09568f9de853b
S <s>
2023-10-10 12:46:38 +0400

    {"types":"C","runs":40}

--- assignment102.java
+++ assignment102.java
@@ -1,4 +1,40 @@
+import stanford.karel.*;
+public class assignment102 extends SuperKarel{
+	public void run() {
+		while(frontIsClear()){
+			fixTheRow();
+			goBack();
+			moveToTheNextRow();
+			}
+		if(frontIsBlocked()) {
+			fixTheRow();
+		}
+	}
 
-public class assignment102 {
 
+	private void moveToTheNextRow() {
+		for(int i = 0; i < 4; i++) {
+			move();
+		}
+	}
+
+	private void goBack() {
+		turnAround();
+		while(frontIsClear()){
+			move();
+		}
+		turnLeft();
+	}
+
+	private void fixTheRow() {
+		turnLeft();
+		while(frontIsClear()) {
+			if(noBeepersPresent()){
+				putBeeper();
+				}else{
+					move();
+				}
+		}
+		
+	}
 }
