01caa7c799979b43c127bc0eb65cace17649f314
S <s>
2023-10-10 17:33:07 +0400

    {"types":"C","runs":66}

--- problem12.java
+++ problem12.java
@@ -3,6 +3,10 @@ import acm.program.GraphicsProgram;
 
 public class problem12 extends GraphicsProgram{
 	public void run() {
-		add (new GOval (100,200), 100, 100);
+		GOval myOval = new GOval(100,200);
+		add(myOval, 100,100);
+		//gOval secondOval = new GOval (100,200);
+		myOval = new GOval(100,200);
+		add(myOval, 200,200);
 	}
 }
