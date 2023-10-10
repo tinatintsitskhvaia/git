af83c9e5295c13e2a637bf5be557fe32e8545cb3
S <s>
2023-10-10 17:45:00 +0400

    {"types":"C","runs":69}

--- problem12.java
+++ problem12.java
@@ -1,3 +1,5 @@
+import java.awt.Color;
+
 import acm.graphics.*;
 import acm.program.GraphicsProgram;
 
@@ -5,8 +7,16 @@ public class problem12 extends GraphicsProgram{
 	public void run() {
 		GOval myOval = new GOval(100,200);
 		add(myOval, 100,100);
-		//gOval secondOval = new GOval (100,200);
+		
 		myOval = new GOval(200,200);
 		add(myOval, 200,200);
+		
+		add(myOval);
+		myOval.setFilled(true);
+		myOval.setColor(Color.RED);
+		
+		
+		
+		
 	}
 }
