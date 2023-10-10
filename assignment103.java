8c3e3746f2daea01d3b9fd32eacdf01b3dc10bc0
S <s>
2023-10-10 13:15:14 +0400

    {"types":"C","runs":42}

--- assignment103.java
+++ assignment103.java
@@ -1,4 +1,24 @@
+import stanford.karel.*;
+public class assignment103 extends SuperKarel{
+	public void run() {
+		while(leftIsClear()){
+			fillLine();
+			moveToTheNextLine();
+		}
+	}
 
-public class assignment103 {
+	private void moveToTheNextLine() {
+		turnLeft();
+		move();
+		turnLeft();	
+	}
 
+	private void fillLine() {
+		while(frontIsClear()){
+			putBeeper();
+			move();
+			move();
+		}
+		
+	}
 }
