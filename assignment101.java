fe9c63e3c9a0ec679971d671b48df3d07351cdd9
S <s>
2023-10-10 11:49:19 +0400

    {"types":"C","runs":37}

--- assignment101.java
+++ assignment101.java
@@ -1,4 +1,30 @@
+import stanford.karel.*;
 
-public class assignment101 {
+public class assignment101 extends SuperKarel {
+	public void run (){
+		leaveTheHouse();
+		pickBeeper();
+		turnAround();
+		goBackToTheInitialPosition();
+	}
 
+	private void leaveTheHouse() {
+		while(frontIsClear()) {
+			move();
+		}
+		turnRight();
+		move();
+		turnLeft();
+		move();
+	}
+
+	private void goBackToTheInitialPosition() {
+		move();
+		turnRight();
+		move();
+		turnLeft();
+		while( frontIsClear()) {
+			move();
+		}
+	}
 }
