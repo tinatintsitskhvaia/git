66d276d37cf62bc10676e508d0155e4dcc9db7c2
S <s>
2023-10-12 19:03:31 +0400

    {"types":"C","runs":128}

--- assignment103.java
+++ assignment103.java
@@ -1,37 +1,59 @@
 import stanford.karel.*;
 public class assignment103 extends SuperKarel{
 	public void run () {
-		if(frontIsClear()){
-			while(frontIsClear()){
-				fillLine();
+		fillLikeCheckBoard();
+	}
+	
+	private void fillLikeCheckBoard() {
+		fillTheRow();
+		while(isThereRow()){
+			if (beepersPresent()) {
+				move();
+				turnLeft();
+				if(facingWest()){
+					move();
+				}else{
+					turnAround();
+					move();
+				}
+			}else{
+				move();
+				turnLeft();
+				if(facingEast()){
+					turnAround();
+				}
 			}
+			fillTheRow();
 		}
-		while(frontIsBlocked()){
-			goBack();
+	}
+
+	private void fillTheRow() {
+		while(frontIsClear()) {
+			putBeeper();
+			if(frontIsClear()){
+				move();
+				if(frontIsClear()) move();
 			}
-		}
-	private void goBack() {
-		turnAround();
-		while(frontIsClear()){
-			move();
 		}
 	}
 	
-
-	private void fillLine() {
-		while(frontIsClear()){
-	
-		putBeeper();
-		if(frontIsClear()){
-			move();
-			if(frontIsClear()){
-				move();
-			
-		}
-
+	private boolean isThereRow() {
+		if(facingEast()) {
+			turnLeft();
+			if(frontIsClear()) {
+				return true;
+			} else {
+				return false;
+			}
+		} else {
+			turnRight();
+			if(frontIsClear()) {
+				return true;
+			} else {
+				return false;
+			}
 		}
 		
 	}
-	}
-
+	
 }
