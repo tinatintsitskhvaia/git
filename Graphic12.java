600dde2be88f2c66418414ff312e645baffbdb17
S <s>
2023-10-12 17:27:45 +0400

    {"types":"C","runs":124}

--- Graphic12.java
+++ Graphic12.java
@@ -1,4 +1,18 @@
+import acm.graphics.*;
+import acm.program.GraphicsProgram;
+import acm.graphics.GLine;
+public class Graphic12  extends GraphicsProgram {
+	public void run() {
+		drawLine();
+			
+	}
 
-public class Graphic12 {
-
+	private void drawLine() {
+		double x1 = 0;
+		double y1 = getHeight();
+	    double x2 = getWidth();
+	    double y2 = y1;
+		GLine line = new GLine( x1, y1, x2, y2);
+		add (line);
+	}
 }
