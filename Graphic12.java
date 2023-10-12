d49ae79908fdbcfb038069c0e49803aef038fbc4
S <s>
2023-10-12 17:30:30 +0400

    {"types":"C","runs":126}

--- Graphic12.java
+++ Graphic12.java
@@ -10,7 +10,7 @@ public class Graphic12  extends GraphicsProgram {
 
 	private void drawLine() {
 		double x1 = 0;
-		double y1 = getHeight();
+		double y1 = getHeight()/2;
 	    double x2 = getWidth();
 	    double y2 = y1;
 		GLine line = new GLine( x1, y1, x2, y2);
