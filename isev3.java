7ce3278e0419c463557f2110b3162ce5c5019977
S <s>
2023-10-13 20:49:56 +0400

    {"types":"C","runs":92}

--- isev3.java
+++ isev3.java
@@ -4,18 +4,26 @@ public class isev3 extends SuperKarel{
 	//es meotxea
 	public void run() {
 		getToTheUpMiddle();
-	    //getDown();
-		//putBeeper();
+	    getDown();
+		putBeeper();
 		
 		
 	}
-   // private void getDown() {
-		//turnAround();
-		//while(frontIsClear()){
-			//move();
-		//}
+   private void getDown() {
+       if(facingEast()){
+    	   turnRight();
+    	   while(frontIsClear()){
+    		   move();
+    	   }
+		}else if(facingNorth()){
+			turnRight();
+			turnRight();
+			while(frontIsClear()){
+				move();
+			}
+		}
 		
-	//}
+	}
 
 	private void getToTheUpMiddle() {
 		while(frontIsClear()&& leftIsClear()){
