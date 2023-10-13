2648e8fdf54549042e1f41c6e220c028cf8e1d27
S <s>
2023-10-13 19:02:33 +0400

    {"types":"C","runs":42}

--- ass13.java
+++ ass13.java
@@ -2,47 +2,11 @@ import stanford.karel.SuperKarel;
 public class ass13 extends SuperKarel{
 	public void run(){
 		fillTheRow();
-		goBack();
-		fillVerticalRow();
-		moveToTheNextVerticalRow();
-		}
+	}
+
 		
-	private void moveToTheNextVerticalRow() {
-		turnAround();
-		while(frontIsClear()){
-			move();
-			turnLeft();
-			fillVerticalRow();
-		}
 		
-	}
-
-	private void fillVerticalRow() {
-		if(beepersPresent()){
-			if(frontIsClear()){
-				move();
-				if(frontIsClear()){
-					move();
-					fillTheRow();
-				}
-			}
-		}else{
-			if(frontIsClear()){
-				move();
-				fillTheRow();
-			}
-		}
-		goBack();
-	}
-
-	private void goBack() {
-		turnAround();
-		while(frontIsClear()){
-			move();
-		}
-		turnRight();
-	}
-
+		
 	private void fillTheRow() {
 		putBeeper();
 		while(frontIsClear()){
@@ -54,6 +18,10 @@ public class ass13 extends SuperKarel{
 				}
 			}
 		}
-		
-	}
+		turnAround();
+		while(frontIsClear()){
+			move();
+		}
+		turnAround();
+}
 }
