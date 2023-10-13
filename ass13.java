063c9809c1edff0e6fad06049a7d0677663df49f
S <s>
2023-10-13 14:20:58 +0400

    {"types":"C","runs":13}

--- ass13.java
+++ ass13.java
@@ -1,38 +1,8 @@
 import stanford.karel.SuperKarel;
 public class ass13 extends SuperKarel{
-	public void run() {
-		while(notFacingNorth()){
-			fillTheRow();
-			moveToTheNextLineIfPossible();
-	}
+	public void run(){
+		fillTheRow();
 }
-	private void moveToTheNextLineIfPossible() {
-		if (facingEast() && leftIsClear()){
-			if(noBeepersPresent()){
-				turnLeft();
-				move();
-				turnLeft();
-			}else{
-				turnLeft();
-				move();
-				turnLeft();
-				move();
-			}
-		}
-		if (facingWest() && rightIsClear()){
-			if(noBeepersPresent()){
-				turnRight();
-				move();
-				turnRight();
-			}else{
-				turnRight();
-				move();
-				turnRight();
-				move();
-			}
-		}
-		
-	}
 
 	private void fillTheRow() {
 		putBeeper();
