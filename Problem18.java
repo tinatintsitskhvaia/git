c443ebd7a1963c84b1adcac5a26a8f828f08d10d
S <s>
2023-10-17 17:55:17 +0400

    {"types":"C","runs":3}

--- Problem18.java
+++ Problem18.java
@@ -1,4 +1,17 @@
-
-public class Problem18 {
+import acm.program.ConsoleProgram;
+public class Problem18 extends ConsoleProgram {
+	public void run () {
+		int num1 = readInt ("Enter number: ");
+		int num2 = readInt (" Enter number: ");
+		
+		if( num1 > num2 ){
+			println(num1);
+		}else{
+			println(num2);
+		}
+		if( num1 == num2 ){
+			println(num1);
+		}
+	}
 
 }
