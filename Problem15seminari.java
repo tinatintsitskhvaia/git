fbf2f2eab26a6fb1cb79400ba9ae95c6339c1a64
S <s>
2023-10-17 17:19:56 +0400

    {"types":"C","runs":1}

--- Problem15seminari.java
+++ Problem15seminari.java
@@ -1,4 +1,13 @@
+import  acm.program.ConsoleProgram;
+public class Problem15seminari extends ConsoleProgram{
+	public void run() {
+		double num1 = readInt ("enter number: ");
+		double num2 = readInt( "enter second number: ");
+		
+		double avg = (num1 + num2) / 2  ;
+		
 
-public class Problem15seminari {
+		
+	}
 
 }
