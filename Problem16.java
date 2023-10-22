7f601059ce94323064c17459198c4847aba8522e
S <s>
2023-10-22 19:58:37 +0400

    {"types":"C","runs":19}

--- Problem16.java
+++ Problem16.java
@@ -1,4 +1,16 @@
+import acm.program.ConsoleProgram;
+public class Problem16 extends ConsoleProgram{
+	public void run() {
+		countHarmonic();
+	}
 
-public class Problem16 {
+	private void countHarmonic() {
+		int num1 = readInt("enter first number: ");
+		int num2 = readInt("enter second number: ");
+		int num3 = readInt("enter third number: ");
+		
+		double harmonic = 1 /(1/num1 + 1/num2 + 1/num3);
+		println("result: " + harmonic);
+	}
 
 }
