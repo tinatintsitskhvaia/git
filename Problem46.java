97125977a15e3e1a53282959efffcfc97e4b0c6d
S <s>
2023-11-17 08:58:42 +0400

    {"types":"C","runs":31}

--- Problem46.java
+++ Problem46.java
@@ -1,4 +1,22 @@
+import acm.program.*;
 
-public class Problem46 {
-
+public class Problem46 extends ConsoleProgram {
+	public void run() {
+		String text = readLine("Enter text:");
+		int maxCount = 0;
+		char maxChar;
+		for (int i = 0; i< text.length(); i++) {
+		}
+		
+		
+	}
+	private int numOccurences (String text, char symbol) {
+		int numOccurences = 0;
+		for(int i = 0; i < text.length(); i++) {
+			if(text.charAt(i) == symbol) {
+				numOccurences++;
+			}
+	}
+		return numOccurences;
+	}
 }
