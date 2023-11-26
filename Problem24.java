844f09123400992234b8e19370e5634ddfe75165
S <s>
2023-11-27 00:01:55 +0400

    {"types":"C","runs":6}

--- Problem24.java
+++ Problem24.java
@@ -1,4 +1,13 @@
-
-public class Problem24 {
+import acm.program.*;
+public class Problem24 extends ConsoleProgram {
+	public void run() {
+		int n = readInt("enter  n ");
+		int divNum = 0;
+		for(int i = 1; i <= n; i++) {
+			if (n%1 ==0){
+				divNum +=1;
+			}
+		}
+	}
 
 }
