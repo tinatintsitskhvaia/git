4289680fd3413df7783839e9f248d0a413731764
S <s>
2023-11-26 23:45:01 +0400

    {"types":"C","runs":5}

--- Problem23.java
+++ Problem23.java
@@ -4,7 +4,7 @@ public class Problem23 extends ConsoleProgram {
 	public void run() {
 		int n = readInt("enter n: ");
 		int sum = 0;
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i <= n; i++) {
 			sum += i;
 		}
 		println(sum);
