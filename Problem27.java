04602d29ec92b5f724e572ae0554a8de778d2755
S <s>
2023-11-27 00:52:04 +0400

    {"types":"C","runs":14}

--- Problem27.java
+++ Problem27.java
@@ -1,4 +1,19 @@
+import acm.program.*;
+public class Problem27 extends ConsoleProgram {
+	public void run() {
+		int n = readInt("enter positive num");
+		int m = readInt("enter positive num");
+		
+		int nPowM = pow(n,m);
+		println(nPowM);
+	}
 
-public class Problem27 {
+	private int pow(int n, int m) {
+		int res = 1;
+		for( int i = 0 ; i < m; i++){
+			res *= i;
+		}
+		return res;
+	}
 
 }
