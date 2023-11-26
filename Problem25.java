6ca62ed2dbfc48c2210bb7fca538bb7c33368e4f
S <s>
2023-11-27 00:32:38 +0400

    {"types":"C","runs":9}

--- Problem25.java
+++ Problem25.java
@@ -1,4 +1,25 @@
+import acm.program.*;
 
-public class Problem25 {
+public class Problem25 extends ConsoleProgram {
+	public void run() {
+		int n = readInt("Enter positive number:  ");
+		int m = readInt("Enter positive number:  ");
+
+		int lcm = lcm(n, m);
+	}
+
+	private int lcm(int n, int m) {
+
+		return (n * m) / gcd(n, m);
+	}
+
+	private int gcd(int n, int m) {
+		for (int i = Math.min(n, m); i >= 1; i--) {
+			if (n % i == 0 && m % 1 == 0) {
+				return i;
+			}
+		}
+		return -1;
+	}
 
 }
