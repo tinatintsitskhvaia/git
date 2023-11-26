26a8b5d67125312172386f0a570fbd63fb109773
S <s>
2023-11-27 00:36:17 +0400

    {"types":"C","runs":12}

--- Problem26.java
+++ Problem26.java
@@ -1,4 +1,19 @@
+import acm.program.*;
+public class Problem26 extends ConsoleProgram{
+	public void run() {
+		int n = readInt("Enter positive number: ");
+		int m = readInt("Enter positive number: ");
 
-public class Problem26 {
+		int gcd= gcd(n, m);
+	}
+
+	private int gcd(int n, int m) {
+		for (int i = Math.min(n, m); i >= 1; i--) {
+			if (n % i == 0 && m % 1 == 0) {
+				return i;
+			}
+		}
+		return -1;
+	}
 
 }
