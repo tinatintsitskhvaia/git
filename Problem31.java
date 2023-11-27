d404f1bc9379470312bbfc0ba31376de7bdc1880
S <s>
2023-11-27 22:47:24 +0400

    {"types":"C","runs":31}

--- Problem31.java
+++ Problem31.java
@@ -7,9 +7,11 @@ public class Problem31 extends ConsoleProgram {
 		for (int i = 2; i <= Math.sqrt(n); i++) {
 			if (n % i == 0) {
 				println(" n is not prime");
-			} 
+			} else{
+				println( "n is prime");
+			}
 		}
-        println( "n is prime");
+        
 	}
 
 }
