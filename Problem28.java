47933458a7e1a11a895288acc46125b3031994bc
S <s>
2023-11-27 21:08:24 +0400

    {"types":"ACR","runs":16}

--- Problem28.java
+++ Problem28.java
@@ -1,4 +1,19 @@
-
-public class Problem28 {
+import acm.program.*;
+public class Problem28 extends ConsoleProgram {
+	private static final int SENTINEL = -1;
+	public void run() {
+		int numEvens = 0;
+		while (true) {
+			int n = readInt("enter n: ");
+			if (n == SENTINEL) {
+				break;
+			}
+			
+			if( n%2 ==0) {
+				numEvens +=1;
+			}
+		}
+		println(numEvens);
+	}
 
 }
