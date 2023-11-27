dcdf70bb7eba5db16d26d9cee7c1feae4a78ec14
S <s>
2023-11-28 00:02:36 +0400

    {"types":"A","runs":4}

---
+++ Problem38.java
@@ -0,0 +1,22 @@
+import acm.program.*;
+import acm.util.RandomGenerator;
+public class Problem38 extends ConsoleProgram{
+	private RandomGenerator rgen = RandomGenerator.getInstance();
+	public void run() {
+		int initialMoney = 1000;
+		while (initialMoney > 0) {
+			int betMoney = readInt ("bet money is: ");
+			int rouletteResult = rgen.nextInt(0, 36);
+			if (betMoney== rouletteResult) {
+				initialMoney = initialMoney + betMoney; 
+				println(" you won" + betMoney +" dollars, now you have"+ initialMoney + " dollars");
+			}else {
+				initialMoney = initialMoney - betMoney;
+				println(" you lost" + betMoney +" dollars, now you have"+ initialMoney + " dollars");
+
+			}
+			
+		}
+	}
+
+}
