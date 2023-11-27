f21ee328139be2f8e182750aa48e36f8ffa42e4c
S <s>
2023-11-27 22:43:37 +0400

    {"types":"C","runs":24}

--- Problem31.java
+++ Problem31.java
@@ -1,4 +1,15 @@
-
-public class Problem31 {
+import acm.program.*;
+public class Problem31 extends ConsoleProgram {
+	public void run() {
+		 int n = readInt ("enter n: ");
+		 
+		 for ( int i = 2; i < Math.sqrt(n); i++) {
+			 if( n % i ==0 ) {
+				 println(" n is not prime");
+			 }
+			 println(" n is prime");
+		 }
+		 
+	}
 
 }
