83cf9f4b2f5a35d6bbcf50d99b885b6af0aa1879
S <s>
2023-11-28 00:54:08 +0400

    {"types":"A","runs":9}

---
+++ Problem40.java
@@ -0,0 +1,29 @@
+import acm.graphics.GOval;
+import acm.program.*;
+public class Problem40 extends GraphicsProgram {
+	private static final int PAUSE_MS = 5;
+	private static final int VEL_X = 3;
+	private static final int RADIUS = 25;
+	public void run() {
+		GOval circle = createCircle();
+		int vx = VEL_X;
+		int vy = 0;
+		
+		while(true) {
+			if( (circle.getX() + 2*RADIUS >= getWidth()) || (circle.getX() < 0)) {
+				vx= -vx;
+			}
+			circle.move(vx, vy);
+			pause(PAUSE_MS);
+		}
+		
+	}
+	private GOval createCircle() {
+		GOval circle = new GOval( 2* RADIUS, 2*RADIUS);
+		circle.setFilled(true);
+		double x = 0;
+		double y = getHeight() /2 - RADIUS;
+		add(circle , x, y);
+		return circle;
+	}
+}
