dd6e6848eed964753e77cbd6f8d97770df99cc13
S <s>
2023-12-01 18:33:14 +0400

    {"types":"A","runs":45}

---
+++ midterm4.java
@@ -0,0 +1,24 @@
+import acm.program.*;
+
+public class midterm4 extends ConsoleProgram {
+	int d;
+
+	public void run() {
+		int n = readInt("n  ");
+		int m = readInt("m  ");
+		if (n == -1 || m == -1) {
+			println("paka");
+		} else {
+			d = m - n;
+		}
+		while (true) {
+			int k = readInt("k ");
+			if (k != -1 && k - m == d) {
+				m = k;
+			}
+			println("ariwmewik progresia");
+		}
+	
+	}
+	
+}
