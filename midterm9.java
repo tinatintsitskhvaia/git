5d474cf3ddf380a1df61d5c8a86f0f5f82208275
S <s>
2023-12-01 22:36:41 +0400

    {"types":"A","runs":69}

---
+++ midterm9.java
@@ -0,0 +1,19 @@
+import acm.program.*;
+
+public class midterm9 extends ConsoleProgram {
+	public void run() {
+
+		int n = readInt();
+
+		for (int i = n - 1; i > 1; i--) {
+			if (n % i == 0) {
+				for (int j = 2; j < i; j++) {
+					continue;
+				}
+			}
+			println(i);
+			
+		}
+		
+	}
+}
