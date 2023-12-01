2a7710c8713bdbb5fc710f407688589628f2a52a
S <s>
2023-12-01 20:21:57 +0400

    {"types":"C","runs":60}

--- midterm6.java
+++ midterm6.java
@@ -1,22 +1,24 @@
 import acm.program.*;
+
 public class midterm6 extends ConsoleProgram {
 	public void run() {
-		String s =" how arxzzxe youuunn";
+		String s = " how arxzzxe youuunn";
 		println(unEvilText(s));
 	}
 
 	private String unEvilText(String s) {
 		String result = "";
-		for (int i = 0; i < s.length(); i++) {
-			char curr = s.charAt(i);
-			if( i < s.length()- 1 && curr == s.charAt(i + 1)){
-				i++;
-			}else{
-				result += curr;
+		while (true) {
+			for (int i = 0; i < s.length(); i++) {
+				char curr = s.charAt(i);
+				if (i < s.length() - 1 && curr == s.charAt(i + 1)) {
+					i++;
+				} else {
+					result += curr;
+				}
 			}
-		}
-		
-		return result;
-	}
 
+			return result;
+		}
+	}
 }
