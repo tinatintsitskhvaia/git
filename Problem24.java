cc864af96463c3385d3f91cb9a10f4c9bca98ce5
S <s>
2023-12-01 23:13:05 +0400

    {"types":"C","runs":73}

--- Problem24.java
+++ Problem24.java
@@ -1,11 +1,12 @@
 import acm.program.*;
+
 public class Problem24 extends ConsoleProgram {
 	public void run() {
 		int n = readInt("enter  n ");
 		int divNum = 0;
-		for(int i = 1; i <= n; i++) {
-			if (n% i ==0){
-				divNum +=1;
+		for (int i = 1; i <= n; i++) {
+			if (n % i == 0) {
+				divNum += 1;
 			}
 		}
 		println(divNum);
--- midterm9.java
+++ midterm9.java
@@ -4,16 +4,31 @@ public class midterm9 extends ConsoleProgram {
 	public void run() {
 
 		int n = readInt();
+		println(getBiggerDivisor(n));
 
-		for (int i = n - 1; i > 1; i--) {
-			if (n % i == 0) {
-				for (int j = 2; j < i; j++) {
-					continue;
+	}
+
+	private int getBiggerDivisor(int n) {
+		int x = 2;
+		for(int i = 2; i <=n; i++){
+			if(n%i ==0){
+				if(check(i)) {
+					x = i;
 				}
-				println(i);
 			}
-			
 		}
-		
+		return x;
+	}
+	
+	private boolean check (int i) {
+		int count = 0;
+		for( int j = 1; j<= i; j++) {
+			if(i %j ==0) {
+				count++;
+			}
+		}
+		if(count>2) return false;
+		return true;
+		 
 	}
 }
