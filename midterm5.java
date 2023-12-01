fcac967f574aee80b0689a56023401fd4b560603
S <s>
2023-12-01 19:35:05 +0400

    {"types":"C","runs":50}

--- midterm5.java
+++ midterm5.java
@@ -1,21 +1,26 @@
 import acm.program.*;
-public class midterm5 extends ConsoleProgram  {
-	public void run() {
-		println(unEvilText("Hoyyw aaare yoxzzxu"));
-		
-	}
 
-	private String unEvilText(String text) {
-		StringBuilder sb = new StringBuilder();
-		while(true){
-			for(int i = 0; i< text.length()-1; i++) {
-				if(text.charAt(i) !=text.charAt(i+1)) {
-					sb.append(text.charAt(i));
+public class midterm5 extends ConsoleProgram {
+	public void run() {
+		int n = readInt();
+		int m = readInt();
+		int q = m /n;
+		boolean check = true;
+		while( n != -1) {
+			n = m;
+			m = readInt();
+			if(m != n* q) {
+				if( m == -1) {
+					break;
 				}
+				check = false;
 			}
 		}
-		sb.append(text.charAt());
-		return sb.toString();
+		if (check) {
+			println("geometr");
+		}else{
+			println("net geometr");
+		}
 	}
-
+	
 }
