6122b5131e19e421376559c3d684fa7e563be032
S <s>
2023-12-01 16:41:05 +0400

    {"types":"A","runs":29}

---
+++ midterm2.java
@@ -0,0 +1,20 @@
+import acm.program.*;
+public class midterm2 extends ConsoleProgram{
+	public void run() {
+		String text = readLine("enter text to compress: ");
+		println("Compressed version of the string is: " + longestRunCompress(text));
+	}
+	int count = 1;
+	String compressedString = ("");
+	private String longestRunCompress(String text) {
+		for( int i = 0; i < text.length(); i ++) {
+			while(text.charAt(i) == text.charAt(i +1)) {
+				count++;				
+			}
+			compressedString = compressedString + count + text.charAt(i);
+			count = 0;
+		}
+		return compressedString;
+	} 
+
+}
