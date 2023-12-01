addb6871bb24d7e748104bd2f1323c81dd069bd1
S <s>
2023-12-01 17:11:20 +0400

    {"types":"C","runs":35}

--- midterm2.java
+++ midterm2.java
@@ -4,6 +4,7 @@ public class midterm2 extends ConsoleProgram {
 	public void run() {
 		String text = readLine("enter text to compress: ");
 		String result = longestRunCompression(text);
+		println(result);
 	}
 
 	private String longestRunCompression(String text) {
@@ -12,16 +13,19 @@ public class midterm2 extends ConsoleProgram {
 		}
 
 		StringBuilder result = new StringBuilder();
+		char currChar = text.charAt(0);
 		int count = 1;
-		for( int i = 0; i < text.length() - 1 ; i++){
-			if (text.charAt(i) == text.charAt(i +1)) {
+		for( int i = 1; i < text.length() ; i++){
+			char nextChar = text.charAt(i);
+			if (nextChar==currChar) {
 				count ++;
 			}else{
 				result.append(count).append(text.charAt(i));
+				currChar = nextChar;
 				count = 1;
 			}
 		}
-				
+		result.append(count).append(currChar);
 		return result.toString();
 	}
 
