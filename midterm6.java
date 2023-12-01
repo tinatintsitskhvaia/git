bd4988c02e2906a6fcf4d326c114dbcac88e24a8
S <s>
2023-12-01 20:20:42 +0400

    {"types":"A","runs":57}

---
+++ midterm6.java
@@ -0,0 +1,22 @@
+import acm.program.*;
+public class midterm6 extends ConsoleProgram {
+	public void run() {
+		String s =" how arxxe youuunn";
+		println(unEvilText(s));
+	}
+
+	private String unEvilText(String s) {
+		String result = "";
+		for (int i = 0; i < s.length(); i++) {
+			char curr = s.charAt(i);
+			if( i < s.length()- 1 && curr == s.charAt(i + 1)){
+				i++;
+			}else{
+				result += curr;
+			}
+		}
+		
+		return result;
+	}
+
+}
