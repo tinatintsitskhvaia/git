8da4843008f6b94b3d0abc6d3e1d86cf79197f5b
S <s>
2024-01-10 19:57:23 +0400

    {"types":"A","runs":5}

---
+++ Problem55.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+
+import acm.program.*;
+
+public class Problem55 extends ConsoleProgram {
+	public void run() {
+		int n = readInt("enter amount:");
+		int[] arr = new int[n];
+		enterNumbers(arr);
+		int[] copy = arr;
+		isEqual(copy, arr);
+
+	}
+
+	private boolean isEqual(int[] copy, int[] arr) {
+		Arrays.sort(arr);
+		for (int i = 0; i < arr.length; i++) {
+			if(arr[i] == copy[i]) {
+				return true;
+			}
+		}
+		return false;
+
+	}
+
+	private void enterNumbers(int[] arr) {
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = readInt("enter number");
+		}
+	}
+
+}
