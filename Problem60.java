968d9fc58b2eae11e3ba47df7b94b0faea3c245d
S <s>
2024-01-22 22:18:32 +0400

    {"types":"C","runs":135}

--- Problem60.java
+++ Problem60.java
@@ -6,22 +6,23 @@ public class Problem60 extends ConsoleProgram {
 	public void run() {
 		while (true) {
 			int n = readInt("enter number: ");
+			enteredNums.add(n);
+			reversedArray(enteredNums);
 			if (n == -1) {
 				break;
 			}
-			enteredNums.add(n);
-			reversedArray(enteredNums);
+
 		}
-		
-		for (int i =0; i< reversed.size(); i++) {
+
+		for (int i = 0; i < reversed.size(); i++) {
 			println(reversed.get(i));
 		}
 	}
 
 	private void reversedArray(ArrayList<Integer> enteredNums) {
-		for (int i = enteredNums.size(); i>0; i--) {
+		for (int i = enteredNums.size(); i > 0; i--) {
 			reversed = new ArrayList<Integer>();
-			reversed.add(enteredNums.get(i-1));	
+			reversed.add(enteredNums.get(i - 1));
 		}
 	}
 
